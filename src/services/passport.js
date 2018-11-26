0f5643a35404dbfc3c1f765c12c3a7caf79ee46a
TatarchukKO <dolbyone007@gmail.com>
2018-11-26 05:43:52 +0300

    Fix heroku proxy issue

--- src/services/passport.js
+++ src/services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
