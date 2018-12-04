9607e0018a87326e557354e2c255cadb583f4c44
maxaderiha <maxaderiha@gmail.com>
2018-12-04 11:23:15 +0300

    Saga setup

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,29 @@
 import { routerMiddleware as createRouterMiddleware } from 'connected-react-router/immutable';
 import * as Immutable from 'immutable';
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { logger } from 'redux-logger';
 
 import history from './configureHistory';
 import createRootReducer from './reducers';
+import rootSaga  from './sagas';
 
 const initialState = Immutable.Map();
 const rootReducer = createRootReducer(history);
+
+const sagaMiddleware = createSagaMiddleware();
 const routerMiddleware = createRouterMiddleware(history);
-const enhancer = applyMiddleware(routerMiddleware, logger);
+
+const enhancer = applyMiddleware(
+  sagaMiddleware,
+  routerMiddleware,
+  logger,
+);
 
 export default createStore(
   rootReducer,
   initialState,
   enhancer,
 );
+
+sagaMiddleware.run(rootSaga);
